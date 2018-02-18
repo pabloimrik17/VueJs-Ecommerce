e6c918ebaacbb56ac4dbb6f95e4b958efbaaced2
Etherless <pabloimrik17@gmail.com>
2018-02-18 20:43:35 +0100

    Added product form component

--- src/pages/admin/NewProduct.vue
+++ src/pages/admin/NewProduct.vue
@@ -1,14 +1,34 @@
 <template>
-  <div>
-    <div class="title">
-      <h1>This is Admin/New</h1>
-    </div>
-  </div>
+  <product-form @save-product="addProduct" :model="model" :manufacturers="manufacturers">
+  </product-form>
 </template>
 
 <script>
+import ProductForm from '../../components/products/ProductForm';
+
 export default {
+  components: { ProductForm },
   name: 'new-product',
+  data() {
+    return {
+      model: {},
+      manufacturers: [
+        {
+          _id: 'sam',
+          name: 'Samsung',
+        },
+        {
+          _id: 'apple',
+          name: 'Apple',
+        },
+      ],
+    };
+  },
+  methods: {
+    addProduct(model) {
+      console.log('model', model);
+    },
+  },
 };
 </script>
 
